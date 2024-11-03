1cfeec4493f32ca0f50eefb4dbac7191d22a015b
Fran2109 <82397975+Fran2109@users.noreply.github.com>
2024-11-03 15:17:11 -0300

    Update BackendApplication.java

--- Backend/src/main/java/com/Astefil_Inflables/Catalogo/Backend/BackendApplication.java
+++ Backend/src/main/java/com/Astefil_Inflables/Catalogo/Backend/BackendApplication.java
@@ -1,7 +1,9 @@
 package com.Astefil_Inflables.Catalogo.Backend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -10,4 +12,10 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run() {
+		return args -> {
+			
+		};
+	}
 }
